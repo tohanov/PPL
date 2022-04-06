28e850a237d562942d8a2cb00f3eee9fb4da8515
tamirosh <80402047+tamirosh@users.noreply.github.com>
2022-04-06 13:37:00 +0300

    questions 2:1,2

--- assignment1/src/part2/part2.ts
+++ assignment1/src/part2/part2.ts
@@ -3,10 +3,22 @@ import * as R from "ramda";
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countLetters: (s: string) => {} = undefined
+export const countLetters: (s: string) => {} =(s)=>{
+   return R.countBy((s_: string)=> R.toLower(s_))(R.filter((letter)=> letter!==" ",stringToArray(s)))
+};
 
 /* Question 2 */
-export const isPaired: (s: string) => boolean = undefined
+export const isPaired: (s: string) => boolean = R.pipe(
+        (sTA: string) => {return stringToArray(sTA); },
+        (find: string[]) => find.reduce((acc, cur) => cur === ')' ? (acc[0] === '(' ? R.drop(1, acc):R.concat(["F"], acc)):
+            cur === ']' ? (acc[0] === '[' ? R.drop(1, acc): R.concat(["F"], acc)):
+            cur === '}' ? (acc[0] === '{' ? R.drop(1, acc): R.concat(["F"], acc)):
+            cur === '(' ? R.concat([cur], acc):
+            cur === '[' ? R.concat([cur], acc):
+            cur === '{' ? R.concat([cur], acc):
+            acc, [""]),
+        (toReturn: string[]) => { return toReturn.length === 1 }
+    );
 
 /* Question 3 */
 export interface WordTree {
