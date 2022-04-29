4f10c559d2bd0091ab933dffb520f94b5da53cc5
tohanov <nope>
2022-04-29 14:09:51 +0300

    added question1 files

--- assignment2/zipCommands.sh
+++ assignment2/zipCommands.sh
@@ -1,8 +1,12 @@
 ID1=123456789
 ID2=123456789
 
-echo
-echo "-====== CHANGE ME ======-"
-echo
+# echo
+# echo "-====== CHANGE ME ======-"
+# echo
 
-# zip -r "${ID1}_${ID2}.zip" "${ID1}_${ID2}.pdf" "src/"
+cp "question1.pdf" "./${ID1}_${ID2}.pdf"
+
+zip -r "${ID1}_${ID2}.zip" "${ID1}_${ID2}.pdf" "src/"
+
+rm -f "./${ID1}_${ID2}.pdf"
