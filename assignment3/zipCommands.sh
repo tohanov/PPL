6975cc5298746687b9fef37865025ca0998ab757
tohanov <nope>
2022-05-02 00:30:50 +0300

    opened a folder for assignment #3

---
+++ assignment3/zipCommands.sh
@@ -0,0 +1,12 @@
+ID1=123456789
+ID2=123456789
+
+echo
+echo "-====== CHANGE ME ======-"
+echo
+
+# cp "question1.pdf" "./${ID1}_${ID2}.pdf"
+
+# zip -r "${ID1}_${ID2}.zip" "${ID1}_${ID2}.pdf" "src/"
+
+# rm -f "./${ID1}_${ID2}.pdf"
